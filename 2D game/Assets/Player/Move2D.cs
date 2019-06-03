a21ad1bf0610d23696be77f59e052a13e22047b6
gert-indrek.poljakov <gert-indrek.poljakov@ametikool.ee>
2019-06-03 10:38:25 +0300

    New movement system

---
+++ 2D game/Assets/Player/Move2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour {
+	public float moveSpeed = 5f;
+	public bool isGrounded = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Jump();
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+		transform.position += movement * Time.deltaTime * moveSpeed;
+	}
+
+	void Jump (){
+		if (Input.GetButtonDown("Jump") && isGrounded == true){
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+		}
+		
+	}
+}
