1a2ea62e00dc6185e8cdf207ec39f28e1b02f61a
eerik.pold <eerik.pold@ametikool.ee>
2019-06-03 10:44:03 +0300

    title22

---
+++ 2D game/Assets/AimScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(
+            Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+            Camera.main.ScreenToWorldPoint(Input.mousePosition).y,
+            transform.position.z
+            );
+	}
+}
---
+++ 2D game/Assets/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var mouse = Input.mousePosition;
+        var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        var offset = new Vector2(mouse.x - screenPoint.x,mouse.y - screenPoint.y);
+        var angle = Mathf.Atan2(offset.y, offset.x)*Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+	}
+}
