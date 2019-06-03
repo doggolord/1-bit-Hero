38cb8a66e270b6b038035c0672f729e487035dd9
doggo <lord>
2019-06-03 21:22:04 +0300

    start of a new era

---
+++ 2D game/Assets/Player/PC.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC : MonoBehaviour
+{
+public float Speed;
+public float jumpForce;
+private float moveInput;
+
+private Rigidbody2D rb;
+
+private bool isGrounded;
+public Transform groundCheck;
+public float checkRadius;
+public LayerMask WhatIsGround;
+
+private int extraJumps;
+public int extraJumpsValue;
+
+    void Start() {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void fixedUpdate() {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, WhatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
+        Debug.Log(moveInput);
+        rb.velocity = new Vector2(moveInput * Speed, rb.velocity.y);
+    }
+
+    void update(){
+
+        if(isGrounded == true){
+            extraJumps = extraJumpsValue;
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0){
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
