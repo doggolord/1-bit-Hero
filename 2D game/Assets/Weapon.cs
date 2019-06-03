260a7dd893baa75d1acf48687934820486b13f89
eerik.pold <eerik.pold@ametikool.ee>
2019-06-03 11:41:03 +0300

    weapon center gravity fix

--- 2D game/Assets/Weapon.cs
+++ 2D game/Assets/Weapon.cs
@@ -11,10 +11,14 @@ public class Weapon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var mouse = Input.mousePosition;
+        /*var mouse = Input.mousePosition;
         var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
         var offset = new Vector2(mouse.x - screenPoint.x,mouse.y - screenPoint.y);
         var angle = Mathf.Atan2(offset.y, offset.x)*Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        transform.rotation = Quaternion.Euler(0, 0, angle);*/
+
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
 	}
 }
