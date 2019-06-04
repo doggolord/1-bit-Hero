b6bde71c181b6318b97cc49d11a332ca66062569
gert-indrek.poljakov <gert-indrek.poljakov@ametikool.ee>
2019-06-04 11:07:29 +0300

    Platforms and AI
    Basic Ai added
    Added 3 more platforms

---
+++ 2D game/Assets/enemy/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+    public float walkSpeed = 1.0f;      // Walkspeed
+    public float wallLeft = 0.0f;       // Define wallLeft
+    public float wallRight = 5.0f;      // Define wallRight
+    float walkingDirection = 1.0f;
+    Vector2 walkAmount;
+    float originalX; // Original float value
+
+
+    void Start () {
+        wallLeft = transform.position.x - 2.5f;
+        wallRight = transform.position.x + 2.5f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+        if (walkingDirection > 0.0f && transform.position.x >= wallRight) {
+            walkingDirection = -1.0f;
+        } else if (walkingDirection < 0.0f && transform.position.x <= wallLeft) {
+            walkingDirection = 1.0f;
+        }
+        transform.Translate(walkAmount);
+    }
+}
