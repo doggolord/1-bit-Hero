22f0aa3b3e72e7a3a080ce9bca12d4b5ec25689e
eerik.pold <eerik.pold@ametikool.ee>
2019-06-04 11:47:05 +0300

    menu

---
+++ 2D game/Assets/MenuManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+
+    public void ToGame()
+    {
+        Application.LoadLevel("gamescene");
+    }
+}
--- 2D game/Assets/Weapon.cs
+++ 2D game/Assets/Weapon.cs
@@ -2,23 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour {
+public class Weapon : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        /*var mouse = Input.mousePosition;
-        var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
-        var offset = new Vector2(mouse.x - screenPoint.x,mouse.y - screenPoint.y);
-        var angle = Mathf.Atan2(offset.y, offset.x)*Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle);*/
+    // Use this for initialization
+    void Start()
+    {
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
-	}
+    }
 }
