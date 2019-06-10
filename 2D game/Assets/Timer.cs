b96514ba2ad65a07b0351a4481903ff50df5c140
eerik.pold <eerik.pold@ametikool.ee>
2019-06-10 14:17:16 +0300

    timer

---
+++ 2D game/Assets/Timer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    float timeLeft = 30.0f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            GameOver();
+        }
+    }
+}
